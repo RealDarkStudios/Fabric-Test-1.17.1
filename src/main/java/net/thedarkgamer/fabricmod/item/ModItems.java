b437a6852fd7d3fcf42d50fd7b94c791cc6576ef
Jonathan Morton <jonathan.x.morton@outlook.com>
2022-01-01 11:43:30 -0600

    Version 1.17.1-0.0.10 (11:43 CST Jan 1, 2022): Add Overworld Exotic Tools, and add Stairs, Slab, Door, Trapdoor, Button, Pressure Plate, Fence, and Fence Gate Loot Table

--- src/main/java/net/thedarkgamer/fabricmod/item/ModItems.java
+++ src/main/java/net/thedarkgamer/fabricmod/item/ModItems.java
@@ -2,12 +2,14 @@ package net.thedarkgamer.fabricmod.item;
 
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 import net.thedarkgamer.fabricmod.FabricTest;
+import net.thedarkgamer.fabricmod.item.custom.ModAxeItem;
+import net.thedarkgamer.fabricmod.item.custom.ModHoeItem;
+import net.thedarkgamer.fabricmod.item.custom.ModPickaxeItem;
 import net.thedarkgamer.fabricmod.item.custom.OreFinderItem;
 
 public class ModItems {
@@ -16,7 +18,12 @@ public class ModItems {
     public static final Item EXOTIC_MATTER = registerItem("overworld_exotic_matter", new Item(new FabricItemSettings().group(ModItemGroups.ITEMS).fireproof().rarity(Rarity.RARE).food(new FoodComponent.Builder().hunger(1).saturationModifier(0.2f).snack().alwaysEdible().build())));
 
     //Tools
-    public static final Item ORE_FINDER = registerItem("ore_finder", new OreFinderItem(new FabricItemSettings().group(ModItemGroups.ITEMS).maxDamage(32).rarity(Rarity.RARE)));
+    public static final Item ORE_FINDER = registerItem("ore_finder", new OreFinderItem(new FabricItemSettings().group(ModItemGroups.TOOLS).maxDamage(32).rarity(Rarity.RARE)));
+    public static final Item EXOTIC_AXE = registerItem("overworld_exotic_axe", new ModAxeItem(ModToolMaterial.EXOTIC_OVERWORLD, 6.0F, -1.9F, new FabricItemSettings().group(ModItemGroups.TOOLS).rarity(Rarity.RARE)));
+    public static final Item EXOTIC_HOE = registerItem("overworld_exotic_hoe", new ModHoeItem(ModToolMaterial.EXOTIC_OVERWORLD, -2, 1.0F, new FabricItemSettings().group(ModItemGroups.TOOLS).rarity(Rarity.RARE)));
+    public static final Item EXOTIC_PICKAXE = registerItem("overworld_exotic_pickaxe", new ModPickaxeItem(ModToolMaterial.EXOTIC_OVERWORLD,1, -1.8F, new FabricItemSettings().group(ModItemGroups.TOOLS).rarity(Rarity.RARE)));
+    public static final Item EXOTIC_SHOVEL = registerItem("overworld_exotic_shovel", new ShovelItem(ModToolMaterial.EXOTIC_OVERWORLD, 1.5F, -3.0F, new FabricItemSettings().group(ModItemGroups.TOOLS).rarity(Rarity.RARE)));
+    public static final Item EXOTIC_SWORD = registerItem("overworld_exotic_sword", new SwordItem(ModToolMaterial.EXOTIC_OVERWORLD, 3, -1.4F, new FabricItemSettings().group(ModItemGroups.TOOLS).rarity(Rarity.RARE)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(FabricTest.MOD_ID, name), item);
@@ -27,3 +34,4 @@ public class ModItems {
     }
 
 }
+
