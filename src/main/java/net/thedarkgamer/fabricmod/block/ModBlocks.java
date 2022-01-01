b6d8003b009004a60eaa3b00c1386a9b8adf66c4
Jonathan Morton <jonathan.x.morton@outlook.com>
2021-12-31 23:39:08 -0600

    Version 1.17.1-0.0.6 (23:39 CST Dec 31, 2021) (LAST UPDATE OF 2021): Add Overworld Exotic Fence, Fence Gate, Stair, and Slab

--- src/main/java/net/thedarkgamer/fabricmod/block/ModBlocks.java
+++ src/main/java/net/thedarkgamer/fabricmod/block/ModBlocks.java
@@ -2,23 +2,26 @@ package net.thedarkgamer.fabricmod.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 import net.thedarkgamer.fabricmod.FabricTest;
-import net.thedarkgamer.fabricmod.block.custom.StatusBlock;
 import net.thedarkgamer.fabricmod.item.ModItemGroups;
+import net.thedarkgamer.fabricmod.block.custom.ModStairsBlock;
 
 public class ModBlocks {
 
     public static final Block OVERWORLD_EXOTIC_ORE = registerBlock("overworld_exotic_ore", new Block(FabricBlockSettings.of(Material.STONE).hardness(2).strength(2).requiresTool()), new FabricItemSettings().rarity(Rarity.RARE).fireproof().group(ModItemGroups.BLOCKS));
     public static final Block OVERWORLD_EXOTIC_BLOCK = registerBlock("overworld_exotic_block", new Block(FabricBlockSettings.of(Material.METAL).hardness(4).strength(3).requiresTool()), new FabricItemSettings().rarity(Rarity.RARE).fireproof().group(ModItemGroups.BLOCKS));
     //public static final Block STATUS_BLOCK = registerBlock("status_block", new StatusBlock(FabricBlockSettings.of(Material.METAL).hardness(1).strength(1).requiresTool()), new FabricItemSettings().rarity(Rarity.RARE).fireproof().group(ModItemGroups.TOOLS));
+    public static final Block OVERWORLD_EXOTIC_STAIRS = registerBlock("overworld_exotic_stairs", new ModStairsBlock(ModBlocks.OVERWORLD_EXOTIC_BLOCK.getDefaultState(), FabricBlockSettings.of(Material.METAL).hardness(4).strength(3).requiresTool()), new FabricItemSettings().rarity(Rarity.RARE).fireproof().group(ModItemGroups.BLOCKS));
+    public static final Block OVERWORLD_EXOTIC_SLAB = registerBlock("overworld_exotic_slab", new SlabBlock(FabricBlockSettings.of(Material.METAL).hardness(4).strength(3).requiresTool()), new FabricItemSettings().rarity(Rarity.RARE).fireproof().group(ModItemGroups.BLOCKS));
+    public static final Block OVERWORLD_EXOTIC_FENCE = registerBlock("overworld_exotic_fence", new FenceBlock(FabricBlockSettings.of(Material.METAL).hardness(4).strength(3).requiresTool()), new FabricItemSettings().rarity(Rarity.RARE).fireproof().group(ModItemGroups.BLOCKS));
+    public static final Block OVERWORLD_EXOTIC_FENCE_GATE = registerBlock("overworld_exotic_fence_gate", new FenceGateBlock(FabricBlockSettings.of(Material.METAL).hardness(4).strength(3).requiresTool()), new FabricItemSettings().rarity(Rarity.RARE).fireproof().group(ModItemGroups.BLOCKS));
+
 
     private static Block registerBlock(String name, Block block, FabricItemSettings settings) {
         registerBlockItem(name, block, settings);
